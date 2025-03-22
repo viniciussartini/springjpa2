dee169e25793adb69f470a97e21bee36f9fc3b96
Vinicius Sartini <sartini.vinicius@outlook.com>
2025-03-22 08:19:24 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/com/project/springjpa2/Springjpa2Application.java
@@ -0,0 +1,13 @@
+package br.com.project.springjpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjpa2Application.class, args);
+	}
+
+}
